2928b71fe1a21d0834463c352bae8ed6ec1361b0
gaballison <allisonfred@gmail.com>
2026-02-09 13:30:29 -0500

    Cleaned up comments and extraneous code

--- WinUpSvcs.ps1
+++ WinUpSvcs.ps1
@@ -16,18 +16,23 @@ $AfServiceNames = 'BITS', 'wuauserv', 'CryptSvc', 'DoSvc', 'UsoSvc', 'TrustedIns
 
 try {
 
-    # Step 01: stop the services 
+    # Step 01: stop the services
+    # 1a. Use foreach to loop through each service name in the $AfServiceNames array
     foreach ($Service in $AfServiceNames)
     {
+        # 1b. Use Stop-Service cmdlet to forcibly stop each service
         Stop-Service -Name $Service -Force
     }
 
     # Step 02: rename the SoftwareDistribution folder to force Windows Updates to download new stuff
-    # ONLY IF folder already exists
+    # 2a. test if SoftwareDistribution folder exists and is a directory
     if (Test-Path -Path "C:\Windows\SoftwareDistribution" -PathType Container) {
+        # 2b. If 2a is TRUE, test if SoftwareDistribution.old exists and is a directory
         if (Test-Path -Path "C:\Windows\SoftwareDistribution.old" -PathType Container) {
+            # 2c. If 2a is TRUE and 2b is TRUE, remove SoftwareDistribution.old
             Remove-Item -Path "C:\Windows\SoftwareDistribution.old" -Recurse -Force
         } else {
+            # 2d. If 2a is TRUE and 2b is FALSE, rename SoftwareDistribution folder to SoftwareDistribution.old
             Rename-Item -Path "C:\Windows\SoftwareDistribution" -NewName "SoftwareDistribution.old" -Force
         }
 
@@ -35,15 +40,16 @@ try {
 
 
     # Step 03: start the services again
+    # 3a. Loop through each service in $AfServiceNames again (now that they've all been stopped)
     foreach ($Service2 in $AfServiceNames) {
 
-        # check if the startup type is Disabled
+        # 3b. Check if the startup type is Disabled
         if ((Get-Service -Name $Service2 -ErrorAction SilentlyContinue).StartType -eq 'Disabled') {
 
-            # if true, set StartupType to Manual and Status to Running
+            # 3c. if 3b is TRUE, set StartupType to Manual and Status to Running
             Set-Service -Name $Service2 -StartupType Manual -Status Running -PassThru
         } else {
-            # if false, we just need to set the Status to Running
+            # 3d. if 3b is FALSE, we just need to set the Status to Running
             Set-Service -Name $Service2 -Status Running -PassThru
 
         }
@@ -52,37 +58,50 @@ try {
 
 }
 catch {
+    # Use the built-in error variable $ to show any errors
     Write-Host "An error occurred: "
     Write-Host $_
 }
 finally {
 
-    # Step 04: Restart services if they are stopped?
+    # Step 04: Start services if they are still stopped
+    # 4a. Loop through each service in $AfServiceNames array
     foreach ($Service3 in $AfServiceNames) {
-        # TO DO: write-host list of services that aren't running
-        # is there a way to forcibly start them??
 
+        # 4b. Check to see if the service's status is Stopped
+        # IMPORTANT: Use single quotes for things like status, startup type, etc.
        if ((Get-Service -Name $Service3 -ErrorAction SilentlyContinue).Status -eq 'Stopped') {
+            # 4c. If 4b is TRUE, use Start-Service cmdlet to start the service
             Start-Service -Name $Service3
        }
 
-        # Get-Service -Name $Service3 | Select-Object -Property DisplayName, StartType, Status
-        
-        # TEST: manually writing each service status
+        # OLD: Get-Service -Name $Service3 | Select-Object -Property DisplayName, StartType, Status
+
+        # 4d. TEST: manually writing each service status
         Write-Host $Service3 "is" (Get-Service -Name $Service3).Status
 
     }
 
     # Step 05: Launch the Services window depending on input
-    $AfOpenSvc = Read-Host -Prompt "Do you want to open the Services window?  [Y] Yes    [N] No   "
+    # 5a. Create variable to hold command line input regarding the services window
+    $AfOpenSvc = Read-Host -Prompt "`n` Do you want to open the Services window?  [Y] Yes    [N] No"
+
+    #5b. Check if input from 5a (when converted to uppercase) is Y
     if($AfOpenSvc.ToUpper() = "Y") {
+
+        #5c. If 5b is TRUE, launch the Services process with administrator priveleges
         Start-Process services.msc -Verb RunAs
     }
     
 
     # Step 06: Launch the Settings app on the Windows Update page so we can try installing updates again
-    $AfOpenSet = Read-Host -Prompt "Do you want to open Windows Updates?  [Y] Yes   [N] No    "
+    # 6a. Create a variable to hold the command line input response regarding the Settings window
+    $AfOpenSet = Read-Host -Prompt "`n` Do you want to open Windows Updates?  [Y] Yes   [N] No"
+
+    # 6b. Check if input from 6a (when converted to uppercase) is Y
     if($AfOpenSet.ToUpper() = "Y") {
+
+        # 6c. If 6b is TRUE, launch the Settings app on the Windows Updtae screen
         Start-Process ms-settings:windowsupdate
     }
 
